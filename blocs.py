ad991ce3669d8ce30957fe5dae614b0f33ff9751
Tristan Gérault <120411182+MTreestanG@users.noreply.github.com>
2023-01-25 15:51:34 +0100

    corrected initialization

--- blocs.py
+++ blocs.py
@@ -693,11 +693,11 @@ def init_dict():
     if not os.path.exists(p):
         os.makedirs(p)
         d = dict(text=['default'], source=['default'], tag=[[]])
-        with open(p + "blocs.pkl", 'wb') as f:
+        with open(p + "\\blocs.pkl", 'wb') as f:
             pickle.dump(d, f)
 
         d = dict(position=[], ID=[], order=[], note=[])
-        with open(p + "plan.pkl", 'wb') as f:
+        with open(p + "\\plan.pkl", 'wb') as f:
             pickle.dump(d, f)
 
 
